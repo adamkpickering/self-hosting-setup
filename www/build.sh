ee6ac10c705e7fadc7976664dff40f65b139babb
Adam Pickering <adamkpickering@gmail.com>
2020-05-08 22:37:05 +0000

    Changes related to using website repo as submodule

--- www/build.sh
+++ www/build.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# pull source from website repository
-# build in this environment?
-# or in dockerfile?
-sudo docker build --tag www -f Dockerfile .
+docker build --tag www -f Dockerfile .
